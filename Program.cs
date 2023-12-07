93846ac00922a8f38245af9235455c86f06ddf77
Andrei Mossakovski <andreimoss@gmail.com>
2023-12-07 18:28:32 +0300

    Revert "This reverts commit e4e7e84f801117c0655435939c3562794df933d3."
    This reverts commit 178551ba3b68baf7a6eec252202aa93986ba7b75.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] x = new int[10,10];
+for(int i=0;i<x.GetLength(0);i++)
+    for(int j=0;j<x.GetLength(1);j++)
+        x[i,j] = new Random().Next(1,100);
+
+void Print(int[,] arr)
+{
+    for(int i=0;i<x.GetLength(0);i++)
+    {
+        for(int j=0;j<x.GetLength(1);j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Print(x);
